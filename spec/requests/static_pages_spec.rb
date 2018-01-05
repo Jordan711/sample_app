229286a493ad0519721d3828b26dbfb7cd6a542a
Jordan <jordanfeng911@gmail.com>
2018-01-05 12:00:21 -0500

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,79 +1,62 @@
-
 require 'spec_helper'
 
 describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title(('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title(('Help')) }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
-    before { visit about_path }
+    before { visit about_path}
 
-    it { should have_content('About') }
-    it { should have_title(('About Us')) }
+    let(:heading) {'About Us'}
+    let(:page_title) {'About Us'}
+
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
-    it { should have_title(('Contact')) }
+    let(:heading) {'Contact'}
+    let(:page_title) {'Contact'}
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
-
-=begin
-describe "StaticPages" do
-
-  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
-
-# The 'Home Page' is a description and rSpec does not care what it says
-  describe "Home page" do
-    it "It should have the content 'Sample App'" do
-    # When I visit the home page, the content should contain the words Sample App
-      visit root_path
-      expect(page).to have_content('Sample App')
-    end
-  end
-
-  describe "Help Page" do
-    it "It should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-  end
-
-  describe "About Page" do
-    it "About Us Page" do
-      visit about_path
-      expect(page).to have_content('About Us')
-    end
-  end
-
-  describe "Contact Page" do
-    it "Contact page content" do
-      visit contact_path
-      expect(page).to have_content("Contact")
-    end
-
-    it "should have the title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
-  end
-
-end
-=end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,7 @@ describe "User Pages -- " do
 
   describe "Sign up Page -- " do
     before {visit signup_path}
-    it {should have_content('Sign up')}
+    it {should have_selector('h1, ''Sign up')}
     it {should have_title('Sign up')}
   end
 end
